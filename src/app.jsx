df442ba5b3ddbe941ca4d69c3da07cd1284d78c0
Manuel Artero Anguita <manutero.developer@gmail.com>
2024-08-21 19:03:46 +0200

    live coding example

--- src/app.jsx
+++ src/app.jsx
@@ -9,19 +9,27 @@ export function App() {
   return (
     <main>
       <div className="element">
-        <img className="icon icon-electric" src={electricIcon} alt="electric type" />
+        <img
+          className="icon icon-electric"
+          src={electricIcon}
+          alt="electric type"
+        />
+        <span className="label">Electric</span>
       </div>
 
       <div className="element">
         <img className="icon icon-ground" src={groundIcon} alt="ground type" />
+        <span className="label">Ground</span>
       </div>
 
       <div className="element">
         <img className="icon icon-ice" src={iceIcon} alt="ice type" />
+        <span className="label">Ice</span>
       </div>
 
       <div className="element">
         <img className="icon icon-water" src={waterIcon} alt="water type" />
+        <span className="label">Water</span>
       </div>
     </main>
   );
